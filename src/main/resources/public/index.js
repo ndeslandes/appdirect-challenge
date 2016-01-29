887c1ab4ecc9e4688384f0f47137682871f71940
Nicolas Deslandes <deslandes.nicolas@gmail.com>
2016-01-29 09:13:43 -0500

    add ui to display users

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -6,7 +6,7 @@ success: function(response){
     var name = response;
     var yourTableHTML = "";
         jQuery.each(name, function(i,data) {
-            $('#userTable').append('<tr><td>' + data.id + '</td><td>' + data.firstname + '</td><td>' + data.lastname + '</td><td>' + data.email + '</td><td>' + data.language + '</td></tr>');
+            $('#userTable').append('<tr><td>' + data.id + '</td><td>' + data.firstName + '</td><td>' + data.lastName + '</td><td>' + data.email + '</td><td>' + data.language + '</td></tr>');
         });
 }
 });
