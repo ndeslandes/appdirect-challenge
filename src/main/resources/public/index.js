2595c67fa50b5a06ae62c408678ed138e2b20e4a
Nicolas Deslandes <deslandes.nicolas@gmail.com>
2016-01-29 15:08:04 -0500

    fix ui

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,12 +1,12 @@
 $.ajax({
 type: 'POST',
-url: '/api/users',
+url: '/api/subscriptions',
 dataType: 'json',
 success: function(response){
     var name = response;
     var yourTableHTML = "";
         jQuery.each(name, function(i,data) {
-            $('#userTable').append('<tr><td>' + data.accountId + '</td><td>' + data.creatorFirstName + '</td><td>' + data.creatorLastName + '</td><td>' + data.edition + '</td></tr>');
+            $('#subscriptionTable').append('<tr><td>' + data.accountId + '</td><td>' + data.creatorFirstName + '</td><td>' + data.creatorLastName + '</td><td>' + data.edition + '</td></tr>');
         });
 }
 });
