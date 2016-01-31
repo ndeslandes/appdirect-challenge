a8e48a13b0decb2fb627ffd49adfdd24f4887f93
Nicolas Deslandes <deslandes.nicolas@gmail.com>
2016-01-31 08:34:49 -0500

    add login

--- src/main/java/org/example/appdirectchallenge/service/UserService.java
+++ src/main/java/org/example/appdirectchallenge/service/UserService.java
@@ -2,18 +2,25 @@ package org.example.appdirectchallenge.service;
 
 import org.example.appdirectchallenge.domain.User;
 import org.example.appdirectchallenge.domain.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.openid.OpenIDAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api")
 public class UserService {
+
+    Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private UserRepository userRepository;
 
@@ -25,7 +32,9 @@ public class UserService {
     @RequestMapping("/user/current")
     public ResponseEntity<User> currentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+        if (authentication != null && authentication instanceof OpenIDAuthenticationToken && authentication.getPrincipal() instanceof UserDetails) {
+            OpenIDAuthenticationToken openIDAuthenticationToken = (OpenIDAuthenticationToken) authentication;
+            logger.info(openIDAuthenticationToken.getAttributes().stream().map(o -> o.getName() + " -> [" + o.getValues().stream().collect(Collectors.joining(", ")) + "]").collect(Collectors.joining(", ")));
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
             User user = userRepository.readByOpenid(userDetails.getUsername());
             return new ResponseEntity<>(user, HttpStatus.OK);
@@ -33,5 +42,4 @@ public class UserService {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 }
